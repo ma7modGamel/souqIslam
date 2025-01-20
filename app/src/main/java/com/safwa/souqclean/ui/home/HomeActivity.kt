1fba61f27d451c1fa515fee6d91b6110119579f4
ma7modGamel <mahmoudgamel10@gmail.com>
2025-01-20 14:22:53 +0300

    create home Activity

--- app/src/main/java/com/safwa/souqclean/ui/home/HomeActivity.kt
+++ app/src/main/java/com/safwa/souqclean/ui/home/HomeActivity.kt
@@ -1,29 +1,54 @@
-package com.safwa.souqclean
+package com.safwa.souqclean.ui.home
 
 import android.animation.ObjectAnimator
 import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.AnticipateInterpolator
-import android.widget.Button
-import android.window.SplashScreen
+import android.widget.Toast
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.safwa.souqclean.R
+import com.safwa.souqclean.data.prefrances.PreferenceDataStoreConstants
+import com.safwa.souqclean.data.prefrances.PreferenceDataStoreHelper
+import com.safwa.souqclean.ui.login.AuthActivity
 import com.safwa.souqclean.utils.CrashlyticsUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity :Activity() {
+class HomeActivity :Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         initSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
+        if (checkUser()){
+
+        }else{
+
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+
+        }
 
     CrashlyticsUtils.sendLogToCrashlytics("OpenCrash","button","key",",crash")
 
 
+    }
+
+    private fun  checkUser() :Boolean {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            PreferenceDataStoreHelper(baseContext).getPreference(
+                PreferenceDataStoreConstants.IS_LOGGED_IN_KEY,
+                false
+            )
+        }
+        return false
     }
 
     private fun initSplashScreen() {
