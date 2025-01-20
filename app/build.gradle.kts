1f9dd3dd4e78b11907c877a472312695963c8df4
ma7modGamel <mahmoudgamel10@gmail.com>
2025-01-20 14:24:10 +0300

    fix issue gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.ksp)
+fix issue//    alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.google.firebase.crashlytics)
-
+//    alias(libs.plugins.proto.buf)
 }
 
 android {
@@ -31,6 +31,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -43,6 +44,30 @@ android {
         dataBinding = true
         viewBinding = true
     }
+
+//    protobuf{
+//
+//        protoc {
+//            artifact = "com.google.protobuf:protoc:${libs.versions.protoc}"
+//        }
+//
+//
+//        generateProtoTasks {
+//            all().forEach { task ->
+////                // فقط توليد Java
+////                task.builtins {
+////                    java {}
+////                }
+//
+//                // إضافة دعم Kotlin
+//                task.plugins {
+//                    kotlin{}
+//                }
+//            }
+//        }
+//
+//
+//    }
 }
 
 dependencies {
@@ -54,17 +79,42 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.crashlytics)
 
-    implementation(libs.ksp.runtime)
+//    implementation(libs.ksp.runtime)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.dynamic.features.fragment)
-    ksp(libs.data.binding.compiler)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
+//    ksp(libs.data.binding.compiler)
 
     //third party lib to observe to network
     implementation(libs.reactivenetwork)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.timber)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.androidx.datastore)
+    implementation(libs.androidx.datastore.core)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.preferences.core)
+    implementation(libs.androidx.datastore.preferences.core.jvm)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+
+//
+//    implementation(libs.protobuf.kotlin)
+//    implementation(libs.protobuf.java)
+
 
 }
