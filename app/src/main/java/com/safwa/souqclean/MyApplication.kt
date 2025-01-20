2a3cfb025724561986d80e1d2877ccac3d059735
ma7modGamel <mahmoudgamel10@gmail.com>
2025-01-20 14:25:17 +0300

    add app class

--- app/src/main/java/com/safwa/souqclean/MyApplication.kt
+++ app/src/main/java/com/safwa/souqclean/MyApplication.kt
@@ -1,13 +1,27 @@
 package com.safwa.souqclean
 
-import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
+import android.os.LocaleList
 import android.util.Log
-
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.github.pwittchen.reactivenetwork.library.ReactiveNetwork
+import com.google.firebase.BuildConfig
 import com.google.firebase.FirebaseApp
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.safwa.souqclean.data.prefrances.PreferenceDataStoreConstants
+import com.safwa.souqclean.data.prefrances.PreferenceDataStoreHelper
+import com.safwa.souqclean.utils.Logger
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import rx.schedulers.Schedulers
+import java.util.Locale
 
 
 //دا الكلاس ال بيفتح اول م ارن الابلكيشن مباشره
@@ -15,17 +29,36 @@ import rx.schedulers.Schedulers
 //عشان كدا باعمل انشيالايز لكل ال باستخدمه فيه
 
 class MyApplication :Application() {
+
+
+
     override fun onCreate() {
+
+        initLanguage()
         super.onCreate()
+        Logger.init(BuildConfig.DEBUG)
         FirebaseApp.initializeApp(this)
         listenToNetworkConnectivity()
 
 
     }
 
+    private fun initLanguage() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val preferenceHelper = PreferenceDataStoreHelper(this@MyApplication)
+            val language = preferenceHelper.getPreference(PreferenceDataStoreConstants.LANGUAGE_KEY, "ar")
+            setLocale(language.toString())
+        }
+    }
+
+    private fun setLocale(languageCode: String) {
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(languageCode))
+    }
 
 
-    fun listenToNetworkConnectivity(){
+
+
+    private fun listenToNetworkConnectivity(){
 
 //        val receiver = ComponentName(this,DeviceBootReciver::class.java)
 //        packageManager.setComponentEnabledSettings(receiver,
@@ -50,6 +83,25 @@ class MyApplication :Application() {
     }
 
 
+/*
+    private fun listenToNetworkConnectivity() {
+        ReactiveNetwork.observeInternetConnectivity()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnDispose {
+                Log.e(TAG, "Network observer disposed")
+            }
+            .subscribe(
+                { isConnected: Boolean ->
+                    Log.e(TAG, "Connection to internet is $isConnected")
+                    FirebaseCrashlytics.getInstance().setCustomKey("connect_to_internet", isConnected)
+                },
+                { throwable ->
+                    Log.e(TAG, "Error observing network connectivity", throwable)
+                }
+            )
+    }
+*/
     companion object{
 
         private const val TAG="MyApplication"
